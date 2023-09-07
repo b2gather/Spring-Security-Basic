996bb8a7102955bdd0ee987fe0ac22f6aea003b6
tlab <b2gather@gmail.com>
2023-09-07 16:11:19 +0900

    로그인 POST 삭제

--- src/main/java/com/tlab/basic/controller/LoginController.java
+++ src/main/java/com/tlab/basic/controller/LoginController.java
@@ -4,7 +4,6 @@ import com.tlab.basic.domain.dto.LoginDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,12 +15,6 @@ public class LoginController {
 		if (!model.containsAttribute("loginDto")) {
 			model.addAttribute("loginDto", new LoginDto());
 		}
-		return "login";
-	}
-
-	@PostMapping("")
-	public String login(LoginDto loginDto) {
-
 		return "login";
 	}
 
